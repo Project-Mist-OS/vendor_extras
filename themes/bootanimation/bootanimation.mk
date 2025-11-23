0dc19aca2542af77d0beb9beb621e94e76353c81
Libra420T <ryantf420@gmail.com>
2025-11-23 14:06:24 -0500

    Bootanimation: Properly Distinguish Bootanimation via Pixels * System partition is required For Tensor Pixels * Use manufacture as flag

--- themes/bootanimation/bootanimation.mk
+++ themes/bootanimation/bootanimation.mk
@@ -1,2 +1,6 @@
 # BootAnimation
-    PRODUCT_COPY_FILES += vendor/extras/themes/bootanimation/bootanimation.zip:$(TARGET_COPY_OUT_PRODUCT)/media/bootanimation.zip
+ifeq ($(TARGET_SOC_NAME), google)
+PRODUCT_COPY_FILES += vendor/extras/themes/bootanimation/bootanimation.zip:$(TARGET_COPY_OUT_SYSTEM)/media/bootanimation.zip
+else
+PRODUCT_COPY_FILES += vendor/extras/themes/bootanimation/bootanimation.zip:$(TARGET_COPY_OUT_PRODUCT)/media/bootanimation.zip
+endif
